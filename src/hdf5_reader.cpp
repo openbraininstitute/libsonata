17dbfdf7d0f7d6e0d1abe9e79b2eef44f7d5bdf6
MikeG <mgevaert@openbraininstitute.org>
2026-04-10 13:37:33 +0200

    Don't use HDF5's default locking behavior when opening files read only (#38)
    * HDF5 uses file locks to implement a `Single Writer, Multiple Reader` model
    * We expect the SONATA HDF5 files to always be finished writing, so we
      don't benefit from this, and on some files systems, there are a finite
      amount of locks (plus it's a pessimizing default)
    * we don't want to rely on `HDF5_USE_FILE_LOCKING` to be set
    * see more info: https://support.hdfgroup.org/documentation/hdf5/latest/_file_lock.html
    * Note: this requires that any other files opened with the HDF5 library
      be opened with the same flags.  Thus, if `h5py` is used to open the file
      concurrently with `libsonata`, there may be failures if the locking
      flags aren't set the same we are using `use_file_locking=True` and
      `ignore_when_disabled=True`

--- src/hdf5_reader.cpp
+++ src/hdf5_reader.cpp
@@ -1,9 +1,35 @@
 #include <bbp/sonata/hdf5_reader.h>
 
 #include "hdf5_reader.hpp"
+namespace {
+class HDF5Lock
+{
+    bool _use_file_locking;
+    bool _ignore_when_disabled;
+
+  public:
+    explicit HDF5Lock(bool use_file_locking, bool ignore_when_disabled)
+        : _use_file_locking(use_file_locking)
+        , _ignore_when_disabled(ignore_when_disabled) { }
+
+    void apply(const hid_t list) const {
+        herr_t err = H5Pset_file_locking(list, _use_file_locking, _ignore_when_disabled);
+        if (err < 0) {
+            HighFive::HDF5ErrMapper::ToException<HighFive::PropertyException>(
+                "Unable to set non-locking when opening file.");
+        }
+    }
+};
+}  // namespace
 
 namespace bbp {
 namespace sonata {
+HighFive::File openHDF5withoutLock(const std::string& path) {
+    HighFive::FileAccessProps fapl = HighFive::FileAccessProps::Default();
+    fapl.add(HDF5Lock(false, true));
+
+    return HighFive::File(path, /*openFlags*/ HighFive::File::AccessMode::ReadOnly, fapl);
+}
 
 
 Hdf5Reader::Hdf5Reader()
