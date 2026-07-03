2b76403530fe93a057064c8091419e9a3e4b9161
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2026-07-03 14:02:15 +0200

    Fix clang-format on python/bindings.cpp (include order)

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -3,11 +3,11 @@
 #include <pybind11/stl.h>
 
 #include <bbp/sonata/common.h>
+#include <bbp/sonata/compartment_sets.h>
 #include <bbp/sonata/config.h>
 #include <bbp/sonata/edges.h>
 #include <bbp/sonata/electrode_reader.h>
 #include <bbp/sonata/node_sets.h>
-#include <bbp/sonata/compartment_sets.h>
 #include <bbp/sonata/nodes.h>
 #include <bbp/sonata/optional.hpp>  //nonstd::optional
 #include <bbp/sonata/report_reader.h>
@@ -1567,43 +1567,46 @@ PYBIND11_MODULE(_libsonata, m) {
     bindReportReader<ElementReportReader, CompartmentID>(m, "Element");
 
     // ElectrodeReader bindings
-    py::class_<ElectrodeDataFrame>(m, "ElectrodeDataFrame",
+    py::class_<ElectrodeDataFrame>(m,
+                                   "ElectrodeDataFrame",
                                    "A container of electrode scaling factor data")
-        .def_property_readonly("ids",
-                               [](const ElectrodeDataFrame& df) {
-                                   std::array<ssize_t, 1> dims{ssize_t(df.ids.size())};
-                                   return managedMemoryArray(df.ids.data(), dims, df);
-                               },
-                               "Row identifiers: (node_id, compartment_index) per row")
-        .def_property_readonly("electrodes",
-                               [](const ElectrodeDataFrame& df) {
-                                   std::array<ssize_t, 1> dims{ssize_t(df.electrodes.size())};
-                                   return managedMemoryArray(df.electrodes.data(), dims, df);
-                               },
-                               "Column identifiers: electrode indices returned")
-        .def_property_readonly("data",
-                               [](const ElectrodeDataFrame& df) {
-                                   std::array<ssize_t, 2> dims{0l,
-                                                               ssize_t(df.electrodes.size())};
-                                   if (dims[1] > 0) {
-                                       dims[0] = df.data.size() / dims[1];
-                                   }
-                                   return managedMemoryArray(df.data.data(), dims, df);
-                               },
-                               "Scaling factor matrix (n_compartments, n_electrodes)");
+        .def_property_readonly(
+            "ids",
+            [](const ElectrodeDataFrame& df) {
+                std::array<ssize_t, 1> dims{ssize_t(df.ids.size())};
+                return managedMemoryArray(df.ids.data(), dims, df);
+            },
+            "Row identifiers: (node_id, compartment_index) per row")
+        .def_property_readonly(
+            "electrodes",
+            [](const ElectrodeDataFrame& df) {
+                std::array<ssize_t, 1> dims{ssize_t(df.electrodes.size())};
+                return managedMemoryArray(df.electrodes.data(), dims, df);
+            },
+            "Column identifiers: electrode indices returned")
+        .def_property_readonly(
+            "data",
+            [](const ElectrodeDataFrame& df) {
+                std::array<ssize_t, 2> dims{0l, ssize_t(df.electrodes.size())};
+                if (dims[1] > 0) {
+                    dims[0] = df.data.size() / dims[1];
+                }
+                return managedMemoryArray(df.data.data(), dims, df);
+            },
+            "Scaling factor matrix (n_compartments, n_electrodes)");
 
-    py::class_<ElectrodeReader::Population>(m, "ElectrodePopulation",
+    py::class_<ElectrodeReader::Population>(m,
+                                            "ElectrodePopulation",
                                             "A population inside an ElectrodeReader")
         .def("get",
              &ElectrodeReader::Population::get,
              "Return scaling factors for selected nodes and electrodes",
              "node_ids"_a = nonstd::nullopt,
              "electrode_ids"_a = nonstd::nullopt)
-        .def_property_readonly("node_ids",
-                               [](const ElectrodeReader::Population& pop) {
-                                   return asArray(pop.getNodeIds());
-                               },
-                               "All node IDs in this population")
+        .def_property_readonly(
+            "node_ids",
+            [](const ElectrodeReader::Population& pop) { return asArray(pop.getNodeIds()); },
+            "All node IDs in this population")
         .def_property_readonly("number_of_electrodes",
                                &ElectrodeReader::Population::getNumberOfElectrodes,
                                "Number of electrodes in this population")
@@ -1625,11 +1628,8 @@ PYBIND11_MODULE(_libsonata, m) {
                                &ElectrodeReader::Population::getElectrodeTypes,
                                "Electrode types ordered by column index");
 
-    py::class_<ElectrodeReader>(m, "ElectrodeReader",
-                                "Reader for SONATA electrode weight files")
-        .def(py::init([](py::object h5_filepath) {
-                 return ElectrodeReader(py::str(h5_filepath));
-             }),
+    py::class_<ElectrodeReader>(m, "ElectrodeReader", "Reader for SONATA electrode weight files")
+        .def(py::init([](py::object h5_filepath) { return ElectrodeReader(py::str(h5_filepath)); }),
              "h5_filepath"_a)
         .def_property_readonly("population_names",
                                &ElectrodeReader::getPopulationNames,
